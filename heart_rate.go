ac749308aae58605a53b62d5f1643ea0a0886db0
ramadani <email.ramadani@gmail.com>
2023-03-15 06:15:14 +0800

    [fb-1] refactor: age variable

--- heart_rate.go
+++ heart_rate.go
@@ -5,7 +5,7 @@ import "time"
 // GetMaxHeartRate 220 - age from dob
 func GetMaxHeartRate(dob time.Time) int {
 	days := time.Since(dob).Hours() / 24
-	year := days / 365
+	age := days / 365
 
-	return 220 - int(year)
+	return 220 - int(age)
 }
